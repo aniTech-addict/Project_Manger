9cc43f825dd388711a5e36eba3582926462f2594
aniTech-addict <kirti7666anand15@gmail.com>
2026-01-17 02:36:09 +0530

    feat(backend): add database initialization and server setup
    - Add initDB.js for MongoDB connection using Mongoose
    - Update index.js to integrate DB init and start server on configured port
    - Include error handling for connection issues

---
+++ backend/src/db/initDB.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+import { config } from '../../config/.env.config.js'
+
+const intiDB = async () => {
+
+    try{
+        await mongoose.connect(config.MONGODB_URI)
+    
+        mongoose.connection.on('connected', () => console.log('connected'));
+
+    } catch (err) {
+        console.log(`Error occurred in db connection, ${err}`)
+    }
+    mongoose.connection.on('error', ()=>{
+        console.log("Error Connecting to db")
+    })
+}
+
+export default intiDB
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,15 @@
-import "express" from 'express'
+import express from 'express'
+import cors from 'cors'
+import { config } from '../config/.env.config.js'
+import intiDB from './db/initDB.js'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+intiDB()
+
+app.listen(config.PORT, () => {
+    console.log(`Server is running on port ${config.PORT}`)
+})
