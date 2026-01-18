883b1c573909d5671d7c5c64ea34de9bf672b412
aniTech-addict <kirti7666anand15@gmail.com>
2026-01-19 00:08:38 +0530

    fix(backend/route):  routes for board, tasks are fized

--- backend/src/routes/boards.route.js
+++ backend/src/routes/boards.route.js
@@ -3,15 +3,15 @@ import asyncHandler from '../helpers/asyncHandler.js'
 import { listBoards, listBoard, listTasks, createTask, createBoard, softDeleteBoard, updateBoard } from '../controllers/boards.controller.js'
 
 export const boardRouter = Router()
-
+// api/v1/boards
 
 
 boardRouter.get('/', asyncHandler(listBoards) )  
 boardRouter.get('/:boardId', asyncHandler(listBoard) )
 
-boardRouter.get('/:taskId', asyncHandler(listTasks) )
+boardRouter.get('/:boardId/tasks', asyncHandler(listTasks) )
 
-boardRouter.post('/taskId', asyncHandler(createTask) ) 
+boardRouter.post('/:boardId/tasks', asyncHandler(createTask) ) // boards/:id/createTask
 boardRouter.post('/', asyncHandler(createBoard))
 
 boardRouter.delete('/:boardId', asyncHandler(softDeleteBoard) )
